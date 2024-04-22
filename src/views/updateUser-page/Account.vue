29517326af08474d24708d543484a5e9f6f714fb
MrXck <1971877559@qq.com>
2024-04-22 14:05:50 +0800

    新增绑定邮箱验证

--- src/views/updateUser-page/Account.vue
+++ src/views/updateUser-page/Account.vue
@@ -22,7 +22,19 @@
         邮箱
       </n-space>
       <n-space>
-        <n-input v-model:value="user.email" placeholder="请输入邮箱" />
+        <n-input v-model:value="email.email" placeholder="请输入邮箱" />
+      </n-space>
+      <n-space>
+        <n-button @click="sendEmail">发送邮件</n-button>
+      </n-space>
+      <n-space v-show="email.sendEmail">
+        邮箱验证码
+      </n-space>
+      <n-space v-show="email.sendEmail">
+        <n-input v-model:value="email.code" placeholder="请输入验证码" />
+      </n-space>
+      <n-space v-show="email.sendEmail">
+        <n-button @click="bindEmail">绑定邮箱</n-button>
       </n-space>
     </n-space>
     <n-space>
@@ -44,7 +56,7 @@
 import {NCard, NSpace, NInput, NButton, useMessage, NSwitch} from 'naive-ui'
 import {ref} from "vue";
 import request from "@/utils/request";
-import {UpdateUserURL} from "@/utils/Constant";
+import {BindUserEmailURL, SendEmailURL, UpdateUserURL} from "@/utils/Constant";
 
 const userInfo = JSON.parse(localStorage.getItem('user'))
 const message = useMessage()
@@ -52,17 +64,50 @@ const emailReg = new RegExp(/^(?=.{1,64}@)[A-Za-z0-9_-]+(?:\.[A-Za-z0-9_-]+)*@[A
 const user = ref({
   username: userInfo.username,
   password: '',
-  email: userInfo.email,
   enableEmail: (!(userInfo.enableEmail === null || userInfo.enableEmail === undefined)),
 })
+const email = ref({
+  email: userInfo.email,
+  code: '',
+  sendEmail: false
+})
 
-function update() {
-  if (user.value.email !== '') {
-    if (!emailReg.test(user.value.email)) {
+function sendEmail() {
+  if (email.value.email !== '') {
+    if (!emailReg.test(email.value.email)) {
       message.error('请输入正确的邮箱')
       return
     }
   }
+  request.get(SendEmailURL + email.value.email).then(res => {
+    if (res.code === 0) {
+      message.success('发送成功')
+      email.value.sendEmail = true
+    } else {
+      message.error(res.msg)
+    }
+  })
+}
+
+function bindEmail() {
+  if (email.value.email !== '') {
+    if (!emailReg.test(email.value.email)) {
+      message.error('请输入正确的邮箱')
+      return
+    }
+  }
+  request.get(`${BindUserEmailURL}/${email.value.email}/${email.value.code}`).then(res => {
+    if (res.code === 0) {
+      message.success('绑定成功')
+      userInfo.email = email.value.email
+      localStorage.setItem('user', JSON.stringify(userInfo))
+    } else {
+      message.error(res.msg)
+    }
+  })
+}
+
+function update() {
   if (user.value.username === '') {
     message.error('请输入用户名')
     return
@@ -76,7 +121,6 @@ function update() {
     if (res.code === 0) {
       message.success('操作成功')
       userInfo.username = user.value.username
-      userInfo.email = user.value.email
       localStorage.setItem('user', JSON.stringify(userInfo))
     } else {
       message.error(res.msg)
